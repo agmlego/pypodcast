7c16671ad67688593a6e1719ab4b2e8dd21b4be5
Jamie Bliss <jamie@ivyleav.es>
2023-12-15 18:30:24 -0500

    Get it downloading one file

--- pypodcast/__init__.py
+++ pypodcast/__init__.py
@@ -1,13 +1,22 @@
+from collections.abc import Mapping
 import concurrent.futures
 import functools
 import itertools
+import mimetypes
 import tempfile
 import traceback
 
+import arrow
 import feedparser
+import mutagen
+import mutagen.mp3
+import mutagen.id3
 import requests
+import rich.console
+import rich.traceback
 
-from .config import open_feedfile, open_cache
+from .config import open_feedfile, open_cache, get_data_dir
+from .junk_drawer import import_string, gentle_format
 
 
 def catch_errors(func):
@@ -16,7 +25,7 @@ def catch_errors(func):
         try:
             return func(*p, **kw)
         except Exception:
-            traceback.print_exc()
+            rich.console.Console().print_exception(show_locals=False)
     return _
 
 
@@ -32,11 +41,14 @@ def process_feeds(pool: concurrent.futures.Executor):
             # TODO: Update feedfile based on HTTP status
 
             for entry in feed.entries:
-                yield pool.submit(process_entry, feed, entry)
+                process_entry(feedconfig, feed, entry)
+                return []
+                # yield pool.submit(process_entry, feedconfig, feed, entry)
 
 
 @catch_errors
 def process_entry(
+    config: Mapping,
     feed: feedparser.FeedParserDict,
     entry: feedparser.FeedParserDict,
 ):
@@ -44,6 +56,7 @@ def process_entry(
         if cache:
             # We've processed this entry. Just exit.
             return
+        print(entry.title)
         # 1. Figure out where the godsdamned audio is
         urls = {
             (bit.type, bit.href)
@@ -70,12 +83,95 @@ def process_entry(
             audiometa = mutagen.File(fileobj=audiobuf)
 
             # 4. Instantiate the provider
-            providercls = ...
-            metadata_provider = providercls(feed, entry, audiometa)
+            providercls = import_string(config['provider'])
+            metadata_provider = providercls(feed.feed, entry, audiometa)
 
+            # 5. Fix up audio tags
+            fix_tags(audiometa, metadata_provider)
+
+            # 6. Write out the audio file
+            audiobuf.seek(0)
+            audiometa.save(fileobj=audiobuf)
+
+            dest = get_data_dir() / (gentle_format(config['filepattern'], metadata_provider) + _get_ext(audiometa.mime))
+            dest.parent.mkdir(parents=True, exist_ok=True)
+            audiobuf.seek(0)
+            dest.write_bytes(audiobuf.read())
+
+
+
+def _get_ext(mimes: list[str]) -> str:
+    for mime in mimes:
+        ext = mimetypes.guess_extension(mime)
+        if ext:
+            return ext
+    else:
+        raise ValueError("No extension found for any of {mimes!r}")
+
+
+def dl_blob(url) -> bytes:
+    """
+    Gets the binary data of a URL
+    """
+    print(f"dl_blob {url=}")
+    resp = requests.get(url)
+    resp.raise_for_status()
+    return resp.content
+
+
+@functools.singledispatch
+def fix_tags(tags, feed, entry):
+    raise NotImplementedError("Unknown format {tags!r}")
+
+
+@fix_tags.register
+def _(tags: mutagen.mp3.MP3, provider):
+    tags['PCST:'] = mutagen.id3.PCST(1)
+    if art := provider.episode_art:
+        print(f"fix_tags {art=}")
+        if isinstance(art, str):
+            # URL, download it
+            art = dl_blob(art)
+
+        tags['APIC:'] = mutagen.id3.APIC(
+            encoding=0,
+            mime='image/jpeg',  # FIXME: Actually pull this from somewhere
+            type=3, desc=u'Track',
+            data=art
+        )
+    if epnum := provider.episode_number:
+        tags['TRCK:'] = mutagen.id3.TRCK(text=str(epnum))
+    if title := provider.episode_title:
+        tags['TIT2:'] = mutagen.id3.TIT2(text=title)
+    if sub := provider.episode_subtitle:
+        tags['TIT3:'] = mutagen.id3.TIT3(text=sub)
+    if summary := provider.summary:
+        tags['COMM:'] = mutagen.id3.COMM(
+            lang='eng',
+            desc=u'Summary',
+            text=summary
+        )
+        tags['TDES:'] = mutagen.id3.TDES(text=summary)
+    if album := provider.album:
+        tags['TALB:'] = mutagen.id3.TALB(text=album)
+    if cat := provider.category:
+        if isinstance(cat, list):
+            cat = ', '.join(cat)
+        tags['TCAT:'] = mutagen.id3.TCAT(text=cat)
+        tags['TCON:'] = mutagen.id3.TCON(text=cat + ', Podcast')
+    else:
+        tags['TCON:'] = mutagen.id3.TCON(text='Podcast')
+    if cr := provider.copyright:
+        tags['TCOP:'] = mutagen.id3.TCOP(text=cr)
+    if pub := provider.pub_date:
+        assert isinstance(pub, arrow.Arrow)
+        tags['TDOR:'] = mutagen.id3.TDOR(text=pub.to('utc').isoformat())
+    if epid := provider.episode_id:
+        tags['TGID:'] = mutagen.id3.TGID(text=epid)
 
 
 def main():
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as pool:
+    rich.traceback.install(show_locals=True)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as pool:
         futs = list(process_feeds(pool))
         concurrent.futures.wait(futs)
--- pypodcast/providers/__init__.py
+++ pypodcast/providers/__init__.py
@@ -1,25 +1,139 @@
+import re
+
+import arrow
+import markdownify
+
+
 class Provider:
-	"""
-	Takes the feed, entry, and audio tags, and condenses it into metadata
-	"""
-	def __init__(self, feed, entry):
-		self.feed = feed
-		self.entry = entry
+    """
+    Takes the feed, entry, and audio tags, and condenses it into metadata
+    """
+    def __init__(self, feed, entry, tags):
+        self.feed = feed
+        self.entry = entry
+        self.tags = tags
 
-	def __getitem__(self, key):
-		try:
-			return getattr(self)
-		except AttributeError:
-			pass
+    def __getitem__(self, key):
+        try:
+            return getattr(self, key)
+        except AttributeError:
+            pass
 
-		try:
-			return self.entry[key]
-		except KeyError:
-			pass
+        try:
+            return self.entry[key]
+        except KeyError:
+            pass
 
-		try:
-			return self.feed[key]
-		except KeyError:
-			pass
+        try:
+            return self.feed[key]
+        except KeyError:
+            pass
 
-		raise KeyError("Cannot find {key!r}")
+        raise KeyError(f"Cannot find {key!r}")
+
+    @property
+    def episode_id(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def episode_art(self) -> str | bytes:
+        raise NotImplementedError
+
+    @property
+    def episode_number(self):
+        raise NotImplementedError
+
+    @property
+    def episode_title(self):
+        raise NotImplementedError
+
+    @property
+    def episode_subtitle(self):
+        raise NotImplementedError
+
+    @property
+    def summary(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def album(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def category(self) -> str | list[str]:
+        raise NotImplementedError
+
+    @property
+    def copyright(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def pub_date(self) -> arrow.Arrow:
+        raise NotImplementedError
+    
+    
+
+
+class Nightvale(Provider):
+    @property
+    def episode_id(self):
+        return self.entry.id
+    
+    @property
+    def episode_art(self):
+        return self.entry.image.href
+
+    @property
+    def episode_number(self):
+        num, _ = self._num_title()
+        return num
+
+    @property
+    def episode_title(self):
+        _, title = self._num_title()
+        return title
+
+    @property
+    def episode_subtitle(self):
+        return self.entry.subtitle
+
+    @property
+    def summary(self):
+        return markdownify.markdownify(self.entry.description)
+
+    def _num_title(self):
+        if match := re.search(r'^(\d+)\s*-\s*(.+)$', self.entry.title):
+            return int(match[1]), match[2]
+        else:
+            return 0, self.entry.title
+
+    @property
+    def album(self):
+        # FIXME: Sort this out better (ANGRY KEYSMASH)
+        num, _ = self._num_title()
+        if num:
+            return "Welcome to Night Vale"
+        else:
+            return ""
+
+    @property
+    def category(self):
+        return ['Fiction', 'Science Fiction']
+
+    @property
+    def copyright(self):
+        return self.feed.copyright
+
+    @property
+    def pub_date(self):
+        return arrow.get(self.entry.published_parsed)
+    
+    
+
+
+class Patreon(Provider):
+    pass
+
+
+class Transistor(Provider):
+    pass
